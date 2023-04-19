05c649d71147f7168c79588d298f7cd17a5205c5
Alex Barboza <NB28649@novabase.pt>
2023-04-19 14:02:01 +0100

    Formatting date coming from API

--- app/src/main/java/com/newsapi/newsfeed/helpers/Helper.kt
+++ app/src/main/java/com/newsapi/newsfeed/helpers/Helper.kt
@@ -1,17 +1,26 @@
 package com.newsapi.newsfeed.helpers
 
-import android.icu.util.Calendar
+import java.text.SimpleDateFormat
 import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
+import java.util.*
 
 class Helper {
     companion object {
-        fun formatDate(date: String): String {
+
+        private const val DATE_PATTERN = "yyyy-MM-dd'T'HH:mm"
+        private const val DATE_START = 0
+        private const val DATE_END = 16
+
+        fun formatDate(dateStr: String): String {
             try {
-                val sanitizedDate = date.substring(0, 16)
-                val format = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'hh:mm")
-                val date: LocalDate = LocalDate.parse(sanitizedDate, format)
-                return "${date.dayOfMonth}/${date.monthValue}/${date.year}"
+                val sanitizedDate = dateStr.substring(DATE_START, DATE_END)
+                val formatter = DateTimeFormatter.ofPattern(DATE_PATTERN)
+                val dateTime = LocalDateTime.parse(sanitizedDate, formatter)
+                return "${dateTime.dayOfMonth}/${dateTime.monthValue}/${dateTime.year} ${dateTime.hour}:${dateTime.minute}"
             } catch (e: java.lang.Exception) {
                 return ""
             }
